3a7ea297ba110b09eaaa459b50ce6983ed4fe0cd
Francesco Fraioli <fraiolinotes@gmail.com>
2018-11-05 09:15:27 -0500

    bootstrap updated

--- js/script.js
+++ js/script.js
@@ -4,14 +4,8 @@ function sticky_relocate() {
     if (window_top > div_top) {
         $('#sidebar').addClass('stick');
         $('#sticky-anchor').height($('#sidebar').outerHeight());
-        $('#world').addClass('small');
-        $('#sphere').height("100");
-        $('#sphere').width("100");
     } else {
         $('#sidebar').removeClass('stick');
         $('#sticky-anchor').height(0);
-        $('#world').removeClass('small');
-        $('#sphere').height("300");
-        $('#sphere').width("300");
     }
 }
