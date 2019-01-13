e011044ef470d70bfd4607fbeb8ee05f8e2e82b0
Francesco Fraioli <pinakes@Frai.lan>
2019-01-13 14:18:57 -0500

    read more functionality added

--- js/script.js
+++ js/script.js
@@ -19,3 +19,24 @@ function StepOnOver(d) {
     	document.getElementById("quote").innerHTML="<p>&ldquo;This instead is the quote for the step 2.&rdquo;<br><small>James Bond, Picture the Homeless</small></p>";
     }
 }
+
+function readMore1(d) {
+    var contentToRead = d + ".html";
+    //var readMoreStepOne = $(document).ready(function(e) {$('#stepOneReadMore');});
+
+    //console.log(readMoreStepOne)
+
+    $('#step1ReadMore').show().load(contentToRead);
+}
+function readMore2(d) {
+    var contentToRead = d + ".html";
+    //var readMoreStepOne = $(document).ready(function(e) {$('#stepOneReadMore');});
+
+    //console.log(readMoreStepOne)
+
+    $('#step2ReadMore').show().load(contentToRead);
+}
+
+function closeReadMore() {
+    $('.readMore').hide();
+}
