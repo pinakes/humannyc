44801ac212e0ebcaa4960e98d1dcbf401567754c
Francesco Fraioli <pinakes@Frai.lan>
2019-01-13 16:13:14 -0500

    steps added to the main path

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top) {
-        $('#sidebar').addClass('stick');
-        $('#sticky-anchor').height($('#sidebar').outerHeight());
-    } else {
-        $('#sidebar').removeClass('stick');
-        $('#sticky-anchor').height(0);
-    }
-}
-
-function StepOnOver(d) {
+function StepOver(d) {
 	switch (d) {
   	case 0:
     	document.getElementById("quote").innerHTML="<p>&ldquo;This is the quote for the first step.&rdquo;<br><small>Jean Rice, Picture the Homeless</small></p>";
