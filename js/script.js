b070c005eaab664a2ad6feafccf82f8d3f13b378
Francesco Fraioli <pinakes@Frai.local>
2019-01-15 18:06:49 -0500

    quotes added and one liners updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 function StepOver(d) {
 	switch (d) {
   	case 0:
-    	document.getElementById("quote").innerHTML="<p>&ldquo;This is the quote for the first step.&rdquo;<br><small>Jean Rice, Picture the Homeless</small></p>";
+    	document.getElementById("quote").innerHTML="";
     	break;
   	case 1:
-    	document.getElementById("quote").innerHTML="<p>&ldquo;This instead is the quote for the step 2.&rdquo;<br><small>James Bond, Picture the Homeless</small></p>";
+    	document.getElementById("quote").innerHTML="<p>&ldquo;You&apos;re better off in the streets than the shelter.&rdquo;</p>";
+        break;
+    case 2:
+        document.getElementById("quote").innerHTML="<p>&ldquo;Why I gotta stay here three more times before you see me and figure out I&apos;m homeless before you help me?&rdquo;</p>";
+        break;
+    case 3:
+        document.getElementById("quote").innerHTML="<p>&ldquo;If the city wants people off the street, why make us wait a whole year?&rdquo;</p>";
+        break;
+    case 4:
+        document.getElementById("quote").innerHTML="<p>&ldquo;It&apos;s not a shelter system that I&apos;m in; it&apos;s a Safe Haven, which is 10 times better--let me tell you--than the shelter system.&rdquo;</p>";
+        break;
+    case 5:
+        document.getElementById("quote").innerHTML="<p>&ldquo;I can&apos;t get my birth certificate, so I can&apos;t really move forward with a lot of the programs. So I&apos;m kind of stuck right now&rdquo;<br><small>Ufemia, Picture The Homeless</small></p>";
+        break;
+    case 6:
+        document.getElementById("quote").innerHTML="<p>&ldquo;He (case manager) basically explained to me that I do not fit any criteria for any voucher or any housing programs that are available.&rdquo;<br><small>Jose, Picture The Homeless</small></p>";
+        break;
+    case 7:
+        document.getElementById("quote").innerHTML="<p>&ldquo;I would make a lot of more supportive housing for people just to get housed. It makes people feel human again.&rdquo;<br><small>Jazmin, Picture The Homeless</small></p>";
+        break;
+    case 8:
+        document.getElementById("quote").innerHTML="<p>&ldquo;You gotta wait for your housing, which takes a year, year and a half again.&rdquo;<br></p>";
     }
+
 }
 
 function readMore1(d) {
